d82bb7552c309d88a8ee1021b9f8d045ec794167
Florian Lebecque <Florianlebecque@hotmail.com>
2023-10-20 12:09:51 +0200

    Implement simple audi analyser -> get level

---
+++ SyncoStronbo/Audio/AudioAnalyser.Android.cs
@@ -0,0 +1,105 @@
+﻿using Android.Media;
+using Android.Media.Metrics;
+using SyncoStronbo.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncoStronbo.Audio
+{
+    public class AudioAnalyser : IAudioAnalyser {
+
+        private bool micOk = false;
+
+        private const int SampleRate = 44100;
+        private const ChannelIn ChannelConfig = ChannelIn.Mono;
+        private const Android.Media.Encoding AudioFormat = Android.Media.Encoding.Pcm16bit;
+        private int BufferSize = AudioRecord.GetMinBufferSize(SampleRate, ChannelConfig, AudioFormat);
+
+        private AudioRecord audioRecord;
+        private bool isRecording;
+        public event EventHandler<double> AudioLevelChanged;
+
+
+        private Thread th;
+
+        private double level;
+
+        public AudioAnalyser() {
+            AudioLevelChanged += AudioChanged;
+
+            th = new Thread(RecordThread);
+
+            level = 0;
+        }
+
+        private void AudioChanged(object sender, double e) {
+            level = e;
+        }
+
+        public async void Init() {
+            PermissionStatus micPermision = await Permissions.CheckStatusAsync<Permissions.Microphone>();
+
+            if (micPermision != PermissionStatus.Granted) {
+                micPermision = await Permissions.RequestAsync<Permissions.Microphone>();
+            }
+
+            if (micPermision != PermissionStatus.Granted) {
+                micOk = false;
+            }
+
+            audioRecord = new AudioRecord(AudioSource.Mic, SampleRate, ChannelConfig, AudioFormat, BufferSize);
+
+            th.Start();
+        }
+
+        private void RecordThread() {
+            if (audioRecord.State == State.Initialized) {
+                isRecording = true;
+                audioRecord.StartRecording();
+
+                byte[] buffer = new byte[BufferSize];
+                while (isRecording) {
+                    audioRecord.Read(buffer, 0, BufferSize);
+                    double audioLevel = CalculateAudioLevel(buffer);
+                    AudioLevelChanged?.Invoke(this, audioLevel);
+                }
+            }
+        }
+
+        private double CalculateAudioLevel(byte[] audioData) {
+            short[] audioSamples = new short[audioData.Length / 2];
+            Buffer.BlockCopy(audioData, 0, audioSamples, 0, audioData.Length);
+
+            double sum = 0.0;
+            foreach (short sample in audioSamples) {
+                sum += Math.Abs(sample);
+            }
+
+            double average = sum / audioSamples.Length;
+            double normalizedLevel = average / short.MaxValue; // Normalize to [0, 1]
+
+            return normalizedLevel;
+        }
+        public double GetHighLevel() {
+            return 0;
+        }
+
+        public double GetLowLevel() {
+            return 0;
+        }
+
+        public double GetMidLevel() {
+
+            if (level < 0) level = 0;
+            if (level > 1) level = 1;
+
+            return level;
+
+        }
+
+       
+    }
+}
---
+++ SyncoStronbo/Audio/AudioAnalyser.Windows.cs
@@ -0,0 +1,74 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncoStronbo.Audio {
+    public class AudioAnalyser : IAudioAnalyser {
+        private WaveInEvent waveIn;
+        private double maxLevel;
+
+
+        private bool micOk = false;
+
+        public async void Init() {
+            PermissionStatus micPermision = await Permissions.CheckStatusAsync<Permissions.Microphone>();
+
+            if(micPermision != PermissionStatus.Granted) {
+                micPermision = await Permissions.RequestAsync<Permissions.Microphone>();
+            }
+
+            if(micPermision != PermissionStatus.Granted) {
+                micOk = false;
+            }
+
+            micOk = true;
+
+            waveIn = new WaveInEvent();
+
+            waveIn.DataAvailable += WaveIn_DataAvailable;
+
+            waveIn.StartRecording();
+
+            maxLevel = 0.0;
+        }
+
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e) {
+            byte[] buffer = e.Buffer;
+            int bytesRead = e.BytesRecorded;
+            short max = 0;
+
+            for (int index = 0; index < bytesRead; index += 2) {
+                short sample = (short)((buffer[index + 1] << 8) | buffer[index]);
+                if (sample > max) {
+                    max = sample;
+                }
+            }
+
+            double level = (max / 32768.0); // Normalize to the range [0, 1]
+            maxLevel = level;
+        }
+
+        public double GetHighLevel() {
+            throw new NotImplementedException();
+        }
+
+        public double GetMidLevel() {
+            if (!micOk) {
+                return 0;
+            }
+
+            if (maxLevel < 0.0) maxLevel = 0.0;
+            if (maxLevel > 1.0) maxLevel = 1.0;
+
+            return maxLevel;
+        }
+
+        public double GetLowLevel() {
+            throw new NotImplementedException();
+        }
+    }
+}
